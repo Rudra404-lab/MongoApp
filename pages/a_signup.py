cbee86473e7890b12a9d84265f514e70892615bd
Rudra404-lab <pratapsingh6931@gmail.com>
2026-07-09 12:13:31 +0530

    Update a_signup.py

--- pages/a_signup.py
+++ pages/a_signup.py
@@ -11,5 +11,5 @@ t4=st.text_input("Email Id")
 t5=st.date_input("DOB")
 b1=st.button("SIGN UP")
 if b1:
-       my.insert_one({"uname":t1,"password":t2,"mobile":str(t3),"email":str(t4),"dob":str(t5)})
+       my.insertOne({"uname":t1,"password":t2,"mobile":str(t3),"email":str(t4),"dob":str(t5)})
        st.success("✅ Sign Up Successfully")
